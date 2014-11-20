f894e80a67c6917bd0f1cd4d806a6e41e80a01ab
hdmetor <hdmetor@gmail.com>
2014-11-20 08:47:54 -0500

    cleaner version of reduce_graph

--- GraphHelpers.py
+++ GraphHelpers.py
@@ -73,6 +73,49 @@ def new_reduce_graph(graph, start, max_depth, max_links = None):
     return visited
 
 
+def new_reduce_graph_2(graph, start, max_depth, max_links = None):
+
+    if start not in graph:
+        return None
+
+    to_visit = [Link(start, depth = 0)]
+    visited = {}
+    while to_visit:
+        page = to_visit.pop(0)
+
+        if page.depth >= max_depth:
+            # we don't need to traverse this node, just save the number of links
+            # in the original graph, if any
+            try:
+                page.weight = len(graph[page.node])
+            except KeyError:
+                page.weight = 0
+
+            # let's make sure we will not visit this node again
+            visited[page.node] = page
+            continue
+
+        else:
+            # we are visiting a new node
+            try:
+                # str(l) to ensure compatibility with imported json files
+                # saving tops max_links links
+                links = [Link(str(l), depth = page.depth + 1) for l in graph[page.node][:max_links]]
+                # number of links in the original graph
+                page.weight = len(graph[page.node])
+                page.sons = links
+                visited[page.node] = page
+                to_visit.extend([l for l in links if l.node not in visited])
+
+
+            except KeyError:
+                # this node in not present in the original graph
+                page.weight = 0
+                visited[page.node] = page
+
+    return visited
+
+
 
 def find_all_links(graph,start):
     to_visit = [start]
